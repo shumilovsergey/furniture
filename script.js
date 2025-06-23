29e3201daa20e8e03703df3311453f23ab415042
shumilov <wumilovsergey@gmail.com>
2025-06-24 01:45:59 +0300

    up

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ async function showSection(section) {
     const aboutHeading = document.createElement('h2');
     aboutHeading.textContent = 'Belsi';
     const aboutText = document.createElement('p');
-    aboutText.textContent = 'Тут можно кратенько написать про магазин';
+    aboutText.textContent = "- российский бренд функциональной дизайнерской мебели с 2011 г. Основатели проекта Белим Наталия и Алексей Сибилев в своей работеруководствуются принципом Функционал.Позитив.Эмоции";
     content.appendChild(aboutHeading);
     content.appendChild(aboutText);
     return;
